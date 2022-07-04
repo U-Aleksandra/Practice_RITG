f083b5fccdc81c30c7422eedeb7bad16bb32fa77
U-Aleksandra <s.ushkva@yandex.ru>
2022-07-04 18:23:23 +0400

    ASP.Net MVC (2 задание)

---
+++ Практика_задание2/Практика_задание2/Controllers/BaseController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Практика_задание2.Controllers
+{
+    public class BaseController : Controller
+    {
+        // GET: Base
+        public string ErrorMessage
+        {
+            get { return TempData["ErrorMessage"] == null ? String.Empty : TempData["ErrorMessage"].ToString(); }
+            set { TempData["ErrorMessage"] = value; }
+        }
+
+        
+        public ActionResult Error()
+        {
+            ViewBag.Message = ErrorMessage;
+            return View();
+            
+        }
+    }
+}
